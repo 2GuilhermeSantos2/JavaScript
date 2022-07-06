54563d3069cc96f7ac739fe0aca384561ab47f13
Guilherme Santos <80928105+DeadMaul@users.noreply.github.com>
2022-07-05 21:14:54 -0300

    05/07/22

---
+++ javascript-promação-web/Aulas/Aula06/js/index.js
@@ -0,0 +1,53 @@
+//let pesssoa = "Guilherme";
+//let pesssoa = "João"
+/*
+let x=10; // Aqui é 10
+
+{
+   let x=2; // Aqui é 2
+}
+
+//Aqui continua a ser 10
+document.getElementById("texto").innerHTML = x
+*/
+
+const x = 10
+
+{
+    const x = 2
+}
+
+document.getElementById("texto").innerHTML = x
+
+
+
+/*
+var pote = "Bombom";
+alert (pote)
+*/
+
+//Declaração de Váriaveis
+//var nome, sobrenome, nome_completo, soma;
+
+//Atribuição dos valores
+/*
+nome = "Guilherme";
+sobrenome = "Santos";
+idade = 20;
+nome_completo = nome + " " + sobrenome;
+*/
+/*
+soma = idade + 10;
+pesssoa= nome + " " + soma;
+*/
+
+//document.getElementById("texto").innerHTML = nome_completo;
+//document.getElementById("texto").innerHTML = pesssoa;
+
+/*
+a=2;
+b=3;
+c=a+b
+
+alert(c)
+*/
---
+++ javascript-promação-web/Aulas/Aula07/js/index.js
@@ -0,0 +1,58 @@
+/*
+1) Operadores Aritméticos (matemáticos);
+
+2) Operadores de Atribuição;
+
+3) Operadores de Sequência (concatenação);
+
+4) Operadores de Comparação;
+
+5) Operador de Condicional(Ternário);
+
+6) Operadores Lógico;
+
+
+
+
+*/
+
+var valor1, valor2, total;
+//valor1 = 10;
+//valor2 = 40;
+/*
+total = ++valor1;
+alert(total);
+alert('O valor um ficou: '+valor1)
+*/
+
+//valor1 += valor2 ;
+
+
+/*
+valor1 -= valor2;
+*/
+/*
+valor1 = "Guilherme"
+valor2 = "Santos"
+total = valor1 + " " + valor2
+*/
+/*
+valor1 = 9;
+valor2 = 9;
+total = (valor1 === valor2);
+alert(total);
+*/ 
+
+var idade, eleitor, resultado
+idade = 65;
+//eleitor = (idade<18) ? "Não eleitor":"Sim, eleitor";
+
+//resultado = (idade > 60 && idade < 70);
+
+//resultado = (idade===65 || idade === 72 );
+
+resultado = !(idade === 65)
+
+//alert('A resposta é: ' + eleitor + ' a idade dele é de: ' + idade + ' anos');
+alert(resultado);
+
